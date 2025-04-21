1ed0ba072443210227972a17acaedd96b3cfe429
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2025-04-21 21:01:10 +0900

    update

--- src/main/java/com/test/project/service/converter/FileConverter.java
+++
@@ -1,7 +0,0 @@
-package com.test.project.service.converter;
-
-public interface FileConverter {
-
-    void convert();
-
-}
---
+++ src/main/java/com/test/project/service/converter/ImageConverter.java
@@ -0,0 +1,9 @@
+package com.test.project.service.converter;
+
+import com.test.project.service.domain.ConvertibleImage;
+
+public interface ImageConverter {
+
+    ConvertibleImage convert(ConvertibleImage convertibleImage);
+
+}
