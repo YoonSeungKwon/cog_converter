2de3c1043106fbe688edf0e962ea5268201faf24
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2025-04-23 19:20:00 +0900

    fix: rename

--- src/main/java/com/test/project/infrastructure/H2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.test.project.infrastructure;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface H2Repository {
-}
--- src/main/java/com/test/project/service/repository/MetaDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.project.service.repository;
-
-import com.test.project.service.domain.ConvertibleImage;
-
-public interface MetaDataRepository {
-
-    void save(ConvertibleImage convertibleImage);
-
-}
